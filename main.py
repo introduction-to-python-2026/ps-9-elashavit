735438a0968885a3f878e0134c766cba35944461
elashavit <ela.shavit@gmail.com>
2026-01-07 00:17:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load data
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv"
-)
-
+df = pd.read_csv("parkinsons.csv")
 df.columns = df.columns.str.strip()
 
-# Feature engineering
 df['freq_range'] = df['MDVP:Fhi(Hz)'] - df['MDVP:Flo(Hz)']
 df['jitter_shimmer_ratio'] = df['MDVP:Jitter(%)'] / (df['MDVP:Shimmer'] + 1e-6)
 df['noise_to_harmonic'] = 1 / (df['HNR'] + 1e-6)
@@ -31,8 +25,7 @@ X = df[selected_features]
 y = df['status']
 
 x_train, x_test, y_train, y_test = train_test_split(
-    X,
-    y,
+    X, y,
     test_size=0.2,
     random_state=42,
     stratify=y
@@ -51,6 +44,8 @@ accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
 joblib.dump(model, "parkinsons_rf.joblib")
+print("Model saved as parkinsons_rf.joblib")
+
 
 
 
