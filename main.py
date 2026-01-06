822f2c116d413c7aa669d8ddaed9b3ce2c026607
elashavit <ela.shavit@gmail.com>
2026-01-07 00:32:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+
+
 import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("parkinsons.csv")
-df.columns = df.columns.str.strip()
 
-df['freq_range'] = df['MDVP:Fhi(Hz)'] - df['MDVP:Flo(Hz)']
-df['jitter_shimmer_ratio'] = df['MDVP:Jitter(%)'] / (df['MDVP:Shimmer'] + 1e-6)
-df['noise_to_harmonic'] = 1 / (df['HNR'] + 1e-6)
-df['complexity_score'] = df['RPDE'] + df['DFA']
 
 selected_features = [
-    'PPE',
+    'MDVP:APQ',
+    'MDVP:PPQ',
+    'NHR',
     'HNR',
-    'freq_range',
-    'jitter_shimmer_ratio',
-    'noise_to_harmonic',
-    'complexity_score'
+    'spread1',
+    'spread2',
+    'D2',
+    'PPE'
 ]
+output_feature = 'status'
 
 X = df[selected_features]
-y = df['status']
+y = df[output_feature]
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+
+
+rom sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(
-    X, y,
+    X_scaled,
+    y,
     test_size=0.2,
-    random_state=42,
-    stratify=y
-)
-
-model = RandomForestClassifier(
-    n_estimators=300,
-    max_depth=8,
     random_state=42
 )
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=10)
 model.fit(x_train, y_train)
+
+
+
+from sklearn.metrics import accuracy_score
 
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
+
 print("Accuracy:", accuracy)
-
-joblib.dump(model, "my_model.joblib")
-
-
-
 
 
 
