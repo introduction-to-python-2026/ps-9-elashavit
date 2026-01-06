4ec3a4af71e570811a870f40211e61d367696a3a
elashavit <ela.shavit@gmail.com>
2026-01-07 00:13:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+
 import pandas as pd
 import joblib
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 # Load data
-df = pd.read_csv("parkinsons.csv")
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv"
+)
+
+df.columns = df.columns.str.strip()
 
 # Feature engineering
 df['freq_range'] = df['MDVP:Fhi(Hz)'] - df['MDVP:Flo(Hz)']
@@ -14,7 +18,6 @@ df['jitter_shimmer_ratio'] = df['MDVP:Jitter(%)'] / (df['MDVP:Shimmer'] + 1e-6)
 df['noise_to_harmonic'] = 1 / (df['HNR'] + 1e-6)
 df['complexity_score'] = df['RPDE'] + df['DFA']
 
-# Selected features
 selected_features = [
     'PPE',
     'HNR',
@@ -27,7 +30,6 @@ selected_features = [
 X = df[selected_features]
 y = df['status']
 
-# Train / test split
 x_train, x_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -36,7 +38,6 @@ x_train, x_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-# Model
 model = RandomForestClassifier(
     n_estimators=300,
     max_depth=8,
@@ -45,13 +46,12 @@ model = RandomForestClassifier(
 
 model.fit(x_train, y_train)
 
-# Evaluation
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
-# Save model
 joblib.dump(model, "parkinsons_rf.joblib")
+
 
 
 
